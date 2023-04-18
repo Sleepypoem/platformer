0e17fcb1a70e27e986dda6a25c88cd2d6c0beb97
Alexander <gallade2014@gmail.com>
2023-04-17 20:42:11 -0600

    Minor bug fixes.

--- src/main/java/com/sleepypoem/platformer/main/GameWindow.java
+++ src/main/java/com/sleepypoem/platformer/main/GameWindow.java
@@ -1,18 +1,16 @@
 package com.sleepypoem.platformer.main;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class GameWindow {
 
     public GameWindow(GamePanel gamePanel) {
         JFrame frame = new JFrame("Platformer");
-        frame.setPreferredSize(new Dimension(800, 600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(true);
+        frame.setResizable(false);
+        frame.add(gamePanel);
         frame.pack();
         frame.setLocationRelativeTo(null);
-        frame.add(gamePanel);
         frame.setVisible(true);
     }
 }
